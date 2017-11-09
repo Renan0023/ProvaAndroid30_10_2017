d556ce2e616de793c4709862cb2f48d0cf6f379b
Renan0023 <renan234@msn.com>
2017-11-09 20:09:26 -0200

    testando aquisdfoigjsdfoígjsdpogjsd

---
+++ app/src/main/java/auladv/com/br/provaandroid30_10_2017/ClasseNova.java
@@ -0,0 +1,9 @@
+package auladv.com.br.provaandroid30_10_2017;
+
+/**
+ * Created by Renan Dutra on 09/11/2017.
+ */
+
+public class ClasseNova {
+    private String varteste = " criei esta classe apenas para testar";
+}
